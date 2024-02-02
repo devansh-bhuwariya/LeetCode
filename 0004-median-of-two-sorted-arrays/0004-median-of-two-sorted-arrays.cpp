4e81fbbafbaeeb7982fbdbffcad5be9143e05d20
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-02-02 21:19:58 +0530

    Time: 30 ms (42.94%), Space: 95 MB (8.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int size = nums1.size() + nums2.size();
+        int mid = size / 2;
         vector<int> merged;
-        double res;
-        for(int i=0;i<nums1.size();i++){
+
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                merged.push_back(nums1[i]);
+                i++;
+            } else {
+                merged.push_back(nums2[j]);
+                j++;
+            }
+
+            if (merged.size() > mid + 1) {
+                break;
+            }
+        }
+
+        while (i < nums1.size() && merged.size() <= mid) {
             merged.push_back(nums1[i]);
+            i++;
         }
-        for(int i=0;i<nums2.size();i++){
-            merged.push_back(nums2[i]);
+
+        while (j < nums2.size() && merged.size() <= mid) {
+            merged.push_back(nums2[j]);
+            j++;
         }
-        sort(merged.begin(),merged.end());
-        if(merged.size()%2==0){
-            res=merged[merged.size()/2]+merged[(merged.size()/2)-1];
-            return res/2.0;
-        }else{
-            return merged[((merged.size()+1)/2)-1];
+
+        if (size % 2 == 0) {
+            return (double)(merged[mid - 1] + merged[mid]) / 2.0;
+        } else {
+            return (double)merged[mid];
         }
     }
 };
