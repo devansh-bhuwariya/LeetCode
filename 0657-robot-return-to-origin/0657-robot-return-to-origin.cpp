c542520eac741d8dee7c896af7c295e15c82fe55
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-02-03 13:12:56 +0530

    Time: 8 ms (84.39%), Space: 9.2 MB (7.80%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        map<char,int> mp;
-        for (int i=0; i<moves.size(); i++){
-            mp[moves[i]]++;
+        int x=0,y=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='U'){
+                y++;
+            }else if(moves[i]=='D'){
+                y--;
+            }else if(moves[i]=='L'){
+                x--;
+            }else if(moves[i]=='R'){
+                x++;
+            }
         }
-        return (mp['L']==mp['R'] && mp['U']==mp['D']);
+        return (x==0 && y==0);
+        
     }
 };
