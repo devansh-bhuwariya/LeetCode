a3896dd39766ab00b0e83c90e8223259774125ee
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-02-06 19:20:33 +0530

    Time: 6 ms (67.67%), Space: 17.5 MB (31.30%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int total=0;
+        for(int i=0;i<operations.size();i++){
+            string s=operations[i];
+            if(s[0]=='+' || s[2]=='+'){
+                total+=1;
+            }else{
+                total-=1;
+            }
+        }
+        return total;
+    }
+};
