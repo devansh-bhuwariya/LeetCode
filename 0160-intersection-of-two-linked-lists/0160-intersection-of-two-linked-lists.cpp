f2eb6baeeab485f48a757aff6aaecbe5c8add2a2
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-03 14:21:50 +0530

    Time: 49 ms (20.23%), Space: 20.9 MB (14.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,46 +8,18 @@
  */
 class Solution {
 public:
-    pair<int,int> getdifference(ListNode* l1,ListNode* l2){
-        int count1=0;
-        int count2=0;
-        while(l1!=nullptr or l2!=nullptr){
-            if(l1!=nullptr){
-                count1+=1;
-                l1=l1->next;
-            }
-            if(l2!=nullptr){
-                count2+=1;
-                l2=l2->next;
-            }
-        }
-        return {count1,count2};
-    }
-    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==nullptr or headB==nullptr){
-            return nullptr;
+        unordered_set<ListNode*>st;
+        while(headA!=nullptr){
+            st.insert(headA);
+            headA=headA->next;
         }
-        ListNode* l1;
-        ListNode* l2;
-        int count1,count2;
-        tie(count1,count2)=getdifference(headA,headB);
-        int diff=max(count1-count2,count2-count1);
-        if(count1>count2){
-            l1=headA;
-            l2=headB;
-        }else{
-            l1=headB;
-            l2=headA; 
+        while(headB!=nullptr){
+            if(st.find(headB)!=st.end()){
+                return headB;
+            }
+            headB=headB->next;
         }
-        while(diff!=0){
-            l1=l1->next;
-            diff--;
-        }
-        while(l1!=l2){
-            l1=l1->next;
-            l2=l2->next;
-        }
-        return l1;
+        return nullptr;
     }
 };
