2814ff4e23f32530ee5431ff31b8d1e05821b94d
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-06 12:41:05 +0530

    Time: 15 ms (93.72%), Space: 20.9 MB (36.68%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,15 +4,15 @@ public:
         int i=0;
         int j=0;
         int count=0;
-        sort(g.begin(),g.end(),greater<int>());
-        sort(s.begin(),s.end(),greater<int>());
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
         while(i<g.size() and j<s.size()){
             if(g[i]<=s[j]){
                 i++;
                 j++;
                 count+=1;
             }else{
-                i++;
+                j++;
             }
         }
         return count;
