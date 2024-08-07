2d257e295c4dff51b4c61f04d64d4c258c70ba7f
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-07 14:41:28 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (62.65%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void recur(int ind,int target,vector<int>& candidates,vector<int>& sub,vector<vector<int>>& result){
+        if(target==0){
+            result.push_back(sub);
+            return;
+        }
+        if(ind==candidates.size()){
+            return;
+        }
+        if(candidates[ind]<=target){
+            sub.push_back(candidates[ind]);
+            recur(ind,target-candidates[ind],candidates,sub,result);
+            sub.pop_back();
+        }
+        recur(ind+1,target,candidates,sub,result);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        vector<int>sub;
+        recur(0,target,candidates,sub,result);
+        return result;
+    }
+};
