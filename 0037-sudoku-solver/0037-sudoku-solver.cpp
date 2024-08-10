192d9c573a9cb7b3382b6d231d0874fcabab79e5
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-10 19:50:04 +0530

    Time: 12 ms (61.49%), Space: 8.1 MB (22.96%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -8,14 +8,14 @@ public:
         return true;
     }
     
-    bool solve(vector<vector<char>>& board){
-        for(int i=0;i<9;i++){
+    bool solve(vector<vector<char>>& board,int row){
+        for(int i=row;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.'){
                     for(char c='1';c<='9';c++){
                         if(isValid(c,board,i,j)){
                             board[i][j]=c;
-                            if(solve(board)){
+                            if(solve(board,i)){
                                 return true;
                             }
                             board[i][j]='.';
@@ -29,6 +29,6 @@ public:
     }
     
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        solve(board,0);
     }
 };
