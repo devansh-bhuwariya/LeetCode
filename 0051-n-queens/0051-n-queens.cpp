8a01c43d60e56ca0a7ea1edfb9b46b65428a3182
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-10 14:01:02 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (75.34%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isValid(int col,int row,vector<string>&board){
-        int c=col;
+        int c=col-1;
         int r=row;
         while(col>=0){
             if(board[row][col]=='Q') return false;
             col--;
         }
         col=c;
-        row=r;
+        row=r+1;
         while(row<board.size() and col>=0){
             if(board[row][col]=='Q') return false;
             row++;
             col--;
         }
         col=c;
-        row=r;
+        row=r-1;
         while(row>=0 and col>=0){
             if(board[row][col]=='Q') return false;
             row--;
