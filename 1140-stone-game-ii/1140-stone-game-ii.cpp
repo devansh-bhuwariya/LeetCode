d7ce195366fbada22ef23e5b0f28055bcda2fe31
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-08-20 23:33:24 +0530

    Time: 9 ms (85.71%), Space: 12.3 MB (66.14%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxStones(vector<int>& suffixSum,int maxTillNow,int currIndex,vector<vector<int>>& memo){
+        if(currIndex+2*maxTillNow>=suffixSum.size()) return suffixSum[currIndex];
+        if(memo[currIndex][maxTillNow]>0) return memo[currIndex][maxTillNow];
+        int res=INT_MAX;
+        for(int i=1;i<=2*maxTillNow;++i){
+            res=min(res,maxStones(suffixSum,max(i,maxTillNow),currIndex+i,memo));
+        }
+        memo[currIndex][maxTillNow]=suffixSum[currIndex]-res;
+        return memo[currIndex][maxTillNow];
+    }
+    
+    int stoneGameII(vector<int>& piles) {
+        vector<int> suffixSum = piles;
+        vector<vector<int>> memo(piles.size(), vector<int>(piles.size()));
+        for(int i=suffixSum.size()-2;i>=0;i--){
+            suffixSum[i]+=suffixSum[i+1];
+        }
+        return maxStones(suffixSum,1,0,memo);
+    }
+};
