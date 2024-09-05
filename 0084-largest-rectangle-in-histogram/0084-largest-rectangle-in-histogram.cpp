1a61e606ac38e3f44e9bcfdb1e1c5000e45c9684
devansh-bhuwariya <122905283+devansh-bhuwariya@users.noreply.github.com>
2024-09-05 19:21:32 +0530

    Time: 88 ms (96.37%), Space: 80 MB (66.69%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    vector<int>nextSmaller(vector<int>arr){
-        vector<int>res(arr.size(),arr.size());
-        stack<int>st;
-        for(int i=arr.size()-1;i>=0;i--){
-            while(!st.empty() and arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if (!st.empty()) {
-                res[i] = st.top();
-            }
-            st.push(i);
-        }
-        return res;
-    }
-    
-    vector<int>previousSmaller(vector<int>arr){
-        vector<int>res(arr.size(),-1);
-        stack<int>st;
-        for(int i=0;i<arr.size();i++){
-            while(!st.empty() and arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if (!st.empty()) {
-                res[i] = st.top();
-            }
-            st.push(i);
-        }
-        return res;
-    }
-    
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse=nextSmaller(heights);
-        vector<int> pse=previousSmaller(heights);
+        stack<int> st;
         int maxArea=INT_MIN;
         for(int i=0;i<heights.size();i++){
-            int area=heights[i]*(nse[i]-pse[i]-1);
+            while(!st.empty() and heights[st.top()]>heights[i]){
+                int x=st.top();
+                st.pop();
+                int pse=(!st.empty())?st.top():-1;
+                int area=heights[x]*(i-pse-1);
+                maxArea=max(area,maxArea);
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int x=st.top();
+            st.pop();
+            int pse=(!st.empty())?st.top():-1;
+            int area=heights[x]*(heights.size()-pse-1);
             maxArea=max(area,maxArea);
         }
         return maxArea;
